3eaad527d7b013176ce1e9b85552f8e5d13a02c5
enWILLYado <enwillyado@hotmail.com>
2019-03-07 19:35:15 +0100

    Internal update to libuv/1.26.1-dev

--- src/3rdparty/libcpuid/libcpuid_util.c
+++ src/3rdparty/libcpuid/libcpuid_util.c
@@ -152,7 +152,11 @@ int match_cpu_codename(const struct match_entry_t* matchtable, int count,
 	int bestindex = 0;
 	int i, t;
 
+#if defined __GNUC__ & (defined _WIN64 | defined _WIN32)
+	debugf(3, "Matching cpu f:%d, m:%d, s:%d, xf:%d, xm:%d, ncore:%d, l2:%d, bcode:%d, bits:%I64d, code:%d\n",
+#else
 	debugf(3, "Matching cpu f:%d, m:%d, s:%d, xf:%d, xm:%d, ncore:%d, l2:%d, bcode:%d, bits:%llu, code:%d\n",
+#endif
 	       data->family, data->model, data->stepping, data->ext_family,
 	       data->ext_model, data->num_cores, data->l2_cache, brand_code, (unsigned long long) bits, model_code);
 
